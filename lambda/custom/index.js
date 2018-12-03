d770bd8ad80c564c95704e766771428f7688b0bf
Anush Patel <33417205+AnushPatel@users.noreply.github.com>
2018-12-03 10:09:36 +0000

    Update index.js

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const Alexa = require('alexa-sdk');
-const story = 'Escape the Office.html';
+const story = 'environment hero.html';
 const TableName = null // story.replace('.html','').replace(/\s/g, "-");
 var $twine = null;
 const linksRegex = /\[\[([^\|\]]*)\|?([^\]]*)\]\]/g;
